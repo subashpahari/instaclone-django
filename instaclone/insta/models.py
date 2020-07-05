a9ef2afe81332ddadf394b54d646d08cdbfa43ba
Subash Pahari <subashpahari.12345@gmail.com>
2020-07-05 12:51:17 +0545

    second  commit

--- instaclone/insta/models.py
+++ instaclone/insta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    caption = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.caption
--- instaclone/insta/views.py
+++ instaclone/insta/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
+
+class PostListView(ListView):
+    template_name = "insta/post_list.html"
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
